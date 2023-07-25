c5c9a610330fe4f862d38ee7071f54abe45808a9
yayesh16 <133193093+yayesh16@users.noreply.github.com>
2023-07-25 14:18:20 +0300

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
-/************************* WRITE HANDLE *************************/
+/************************** WRITE HANDLE **************************/
 /**
- * handle_write_char - Prints a string
+ * handle_write_char - Prints a string in this code
  * @c: char types.
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags.
