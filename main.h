13816f71cef6dfe355dbe3ad8123568c9b71b994
yayesh16 <133193093+yayesh16@users.noreply.github.com>
2023-07-25 14:15:48 +0300

    Update main.h

--- main.h
+++ main.h
@@ -43,9 +43,9 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/****************** FUNCTIONS ******************/
+/******************* FUNCTIONS *******************/
 
-/* Funtions to print chars and strings */
+/* Funtions to print chars and strings on this code */
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
@@ -106,7 +106,7 @@ int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
 
-/****************** UTILS ******************/
+/******************* UTILS *******************/
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
